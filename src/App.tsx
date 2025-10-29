eb8fe2934c3878d6fa6f75ad1ad2df1d394bcdf8
Tony M <111037456+m0th3rfkr@users.noreply.github.com>
2025-10-29 10:48:23 -0600

    Add Tourism route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Teams from "./pages/Teams";
 import Matches from "./pages/Matches";
 import News from "./pages/News";
 import Auth from "./pages/Auth";
+import Tourism from "./pages/Tourism";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -23,6 +24,7 @@ const App = () => (
           <Route path="/teams" element={<Teams />} />
           <Route path="/matches" element={<Matches />} />
           <Route path="/news" element={<News />} />
+          <Route path="/tourism" element={<Tourism />} />
           <Route path="/auth" element={<Auth />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
